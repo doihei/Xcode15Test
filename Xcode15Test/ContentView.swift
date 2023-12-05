dcbc56004372b98dc21d2bf3848fd3309eb07695
daiheidoi <dd11197208@gmail.com>
2023-12-05 11:22:26 +0900

    Initial Commit

---
+++ Xcode15Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode15Test
+//
+//  Created by Doi Daihei on 2023/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode15Test/Xcode15TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode15TestApp.swift
+//  Xcode15Test
+//
+//  Created by Doi Daihei on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct Xcode15TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
